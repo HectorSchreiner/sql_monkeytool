6ac02d3efe817d46f9f43216e08554b19cff7c68
HectorSchreiner <agenthector007@gmail.com>
2024-11-01 14:47:25 +0100

    init

---
+++ sql_monkeytool/target/debug/deps/sql_monkeytool-28132d2bfbc07e6f.d
@@ -0,0 +1,5 @@
+/home/hector/Programming/Rust/sql_monkeytool/sql_monkeytool/target/debug/deps/sql_monkeytool-28132d2bfbc07e6f: src/main.rs
+
+/home/hector/Programming/Rust/sql_monkeytool/sql_monkeytool/target/debug/deps/sql_monkeytool-28132d2bfbc07e6f.d: src/main.rs
+
+src/main.rs:
---
+++ sql_monkeytool/target/debug/sql_monkeytool.d
@@ -0,0 +1,1 @@
+/home/hector/Programming/Rust/sql_monkeytool/sql_monkeytool/target/debug/sql_monkeytool: /home/hector/Programming/Rust/sql_monkeytool/sql_monkeytool/src/main.rs
